85eda2970356d64cc03860f0bb537c464adc99fd
danielggc <danielgrecia7@gmail.com>
2021-03-19 09:30:17 -0500

    creando funciones decremento y set

--- raspberry_control/main.py
+++ raspberry_control/main.py
@@ -1,13 +1,22 @@
 from flask import Flask,request
 app = Flask(__name__)
 
-@app.route('/volum/increment')
+@app.route('/volum/increment',methods=['PUT','POST'])
 
-def holaMundo():
-    valor=request.args.get('value')
-    print(valor)
+def increment():
+    value=request.args.get('value')
+    print("increment"+value)
     return ''
+@app.route('/volum/decrement',methods=['PUT','POST'])
+def decrement():
+    Value =request.args.get('decrement')
+    print("decrement:"+value)
+@app.route('/volum/set',methods=['PUT','POST'])
+def volumSet():
+    Value =request.args.get('set')
+    print("ser:"+value)
 @app.after_request
 def apply_caching(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
     return response
