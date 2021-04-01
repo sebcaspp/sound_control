ba686335b777165a2e094ceb16be73acd24ad20b
danielggc <danielgrecia7@gmail.com>
2021-04-01 13:40:47 -0500

    realzando cabios para primera prueba

--- raspberry_control/control/facade.py
+++ raspberry_control/control/facade.py
@@ -1,22 +1,29 @@
 from flask import Flask,request
+import control.infrastructure.volum_control import VolumeControl
 
 app = Flask(__name__)
+
+volum_control = VolumeControl(0)
 
 @app.route('/volum/increment',methods=['PUT','POST'])
 def increment():
     value=request.args.get('value')
     print("increment"+value)
+    volum_control.increase_volume()
     return ''
 
 @app.route('/volum/decrement',methods=['PUT','POST'])
 def decrement():
     value = request.args.get('decrement')
     print("decrement:"+value)
+    volum_control.decrease_volume()
+
 
 @app.route('/volum/set',methods=['PUT','POST'])
 def volumSet():
     value = request.args.get('set')
-    print("ser:"+value)
+    print("set:"+value)
+    volum_control.set_volume(int(value))
 
 @app.after_request
 def apply_caching(response):
