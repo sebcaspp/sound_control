b3f93a4d29efa008d3a252ce1f58b2e40f5ffa37
danielggc <danielgrecia7@gmail.com>
2021-04-15 21:44:00 -0500

    identacion corregida 2

--- raspberry_control/control/infrastructure/volum_control.py
+++ raspberry_control/control/infrastructure/volum_control.py
@@ -4,13 +4,13 @@ from time import sleep
 
 class VolumeControl():
     def __init__(self, volume_value):
+        self.decrement_to_zero()        
         self.gpio_controller = get_gpio_controller()
         self.volume_value = 0
         self.up_down = self.gpio_controller.up_down
         self.inc     = self.gpio_controller.inc
         self.set_volume(volume_value)
-        self.decrementToZero()
-
+        
     def increase_volume(self):
         self.gpio_controller.set_output_hight(self.up_down)
         self.gpio_controller.toggle_output(self.inc)
@@ -32,10 +32,11 @@ class VolumeControl():
             while self.volume_value != value: 
                 self.decrease_volume()  
     
-    def decrement_to_zero():   
+    def decrement_to_zero(self):   
         for i in range(100):
-            self.decrement();            
-    def mute():
+            self.decrease_volume()      
+      
+    def mute(self):
         pass
 
 
